f52feb30db2f8d6ba94e535fa890becac79b7577
Learning100 <140375707+SoloParaSaber@users.noreply.github.com>
2024-02-23 18:38:28 -0700

    Java Programs
    Java programs I created.

---
+++ FirstJavaProgam.java
@@ -0,0 +1,24 @@
+//---------------------------------------
+// Programmer:		Candido Renteria
+// Course:			COSC 1336 Section 013
+// Semester:		Spring 2023
+// Assignment #:	1a
+// Due Date: 		February 1, 2023
+//---------------------------------------
+
+
+public class FirstJavaProgam {
+
+	public static void main(String[] args) {
+		System.out.println("This is my first Java program!");
+		System.out.println();
+		System.out.println("This program was written by Candido Renteria.");
+		System.out.println("End of program.");
+		
+		
+		
+		
+		
+	}	// end method main
+
+}	// end class FirstJavaProgram
---
+++ Quotes.java
@@ -0,0 +1,27 @@
+//---------------------------------------
+// Programmer:		Candido Renteria
+// Course:			COSC 1336 Section 013
+// Semester:		Spring 2023
+// Assignment #:	1b
+// Due Date: 		February 1, 2023
+//---------------------------------------
+
+
+public class Quotes {
+
+	public static void main(String[] args) {
+		System.out.println("To be,");
+		System.out.println(" or not to be,");
+		System.out.println(" that is the question.");
+		System.out.println();
+		System.out.println("Or perhaps,");
+		System.out.println("\tI should be asking,");
+		System.out.println("\tsome other question!\n");
+		System.out.println("This program was written by Candido Renteria.");
+		System.out.println("End of program.");
+		
+		
+		
+	}	// end method main
+
+}	// end class Quotes
